a6c8f93010d7ddfb5d506c9661091daf12c4771f
Usama Mohamed <usamamohamed1222@gmail.com>
2023-01-24 17:27:30 +0200

    WEATHER_APP_v1

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '084382f09fmsh98f59dfb2684f54p10d69ejsn81a4c01b4fb7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0955dcad3de80e6bbac2515e7a78e016";
